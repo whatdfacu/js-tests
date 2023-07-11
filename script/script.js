1dc658a81e192bf9f7c2a195baac4faa5d89509a
Facundo Conde <facundo.conde92@gmail.com>
2023-07-11 12:49:52 -0300

    clean script

--- script/script.js
+++ script/script.js
@@ -1,52 +1,14 @@
-var pic1 = document.getElementById("pic-1");
+const pic1 = document.getElementById("pic-1");
+const pic2 = document.getElementById("pic-2");
+const pic1Tag = '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">';
+const pic2Tag = '<img class="pic" src="img/bastion_3b_restorationkid.jpg" alt="picture2" alt="pic2">';;
 
-var pic2 = document.getElementById("pic-2");
-
-pic1Tag = '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">';
-
-pic2Tag = '<img class="pic" src="img/bastion_3b_restorationkid.jpg" alt="picture2" alt="pic2">';
-
-function change1 () {
-
-    pic1.innerHTML = '<img class="pic" src="img/bastion_3b_restorationkid.jpg" alt="picture2" alt="pic2">';
-    
-}
-
-function change2 () {
-
-    pic2.innerHTML = '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">';
-    
-}
-function alternatePic1 () {
-    if (pic1.innerHTML == '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">') {
-
-        pic1.innerHTML = '<img class="pic" src="img/bastion_3b_restorationkid.jpg" alt="picture2" alt="pic2">';
-
-    }
-    else {
-
-        pic1.innerHTML = '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">';
-
-    };
+function alternatePic (pic) {
+    if(pic.innerHTML === pic1Tag)
+    {pic.innerHTML = pic2Tag;}
+    else
+    {pic.innerHTML = pic1Tag;};
 };
 
-function alternatePic2 () {
-    if (pic2.innerHTML == '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">') {
-
-        pic2.innerHTML = '<img class="pic" src="img/bastion_3b_restorationkid.jpg" alt="picture2" alt="pic2">';
-
-    }
-    else {
-
-        pic2.innerHTML = '<img class="pic" src="img/2pexels-pixabay-355887.jpg" alt="pic1">';
-
-    };
-};
-
-pic1.onclick = function(){
-    alternatePic1();
-}
-
-pic2.onclick = function(){
-    alternatePic2();
-}
+pic1.onclick = function(){alternatePic(pic1);}
+pic2.onclick = function(){alternatePic(pic2);}
