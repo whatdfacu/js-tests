ef8da1e6b1889761a08c0449f65a54b0fdf0806d
Facundo Conde <facundo.conde92@gmail.com>
2023-07-13 15:09:36 -0300

    clean

--- script/script.js
+++ script/script.js
@@ -1,36 +1,41 @@
 const pic1 = document.getElementById("pic-1");
 const pic2 = document.getElementById("pic-2");
-const pic1Tag = '<img class="pic" src="img/FOTO1.jpg" alt="pic1">';
-const pic2Tag = '<img class="pic" src="img/FOTO2.jpg" alt="pic2">';
-const pic3Tag = '<div id="pic-3"><img class="pic" src="img/FOTO3.jpg" alt="pic3"></div>';
-
+const pic3 = document.getElementById("pic-3");
 var foto = 2;
-
-function alternatePic (pic) {
-    if(foto < 6)
-    {pic.innerHTML = '<img class="pic" src="img/FOTO'+foto+'.jpg" alt="pic1">'; foto = foto+1}
-    else
-    {pic.innerHTML = '<img class="pic" src="img/FOTO1.jpg" alt="pic1">'; foto = 2};
-};
-
-pic1.onclick = function(){alternatePic(pic1);};
-pic2.onclick = function(){alternatePic(pic2);};
-
 var button = document.getElementById("button");
 var newFlex = document.getElementById("new-flex");
-var newFlexContent = '<div><p>hi, the loop is: </p></div>';
-var backroundFlex = document.getElementById("body")
+var newFlexContent = "<div><p>hi, the loop is: </p></div>";
+var backroundFlex = document.getElementById("body");
 
-function newFlexGo(){
+function alternatePic(pic) {
+  if (foto < 6) {
+    pic.innerHTML =
+      '<img class="pic" src="img/FOTO' + foto + '.jpg" alt="pic1">';
+    foto = foto + 1;
+  } else {
+    pic.innerHTML = '<img class="pic" src="img/FOTO1.jpg" alt="pic1">';
+    foto = 2;
+  }
+}
 
-    var lupin = 1;    
-    while(lupin <= 10){
+function newFlexGo() {
+  var lupin = 1;
+  while (lupin <= 5) {
+    console.log(lupin);
+    newFlex.innerHTML=newFlex.innerHTML+newFlexContent+lupin+'<img class="pic" src="img/FOTO'+lupin+'.jpg" alt="pic1">';
+    lupin = lupin + 1;
+  }
+}
 
-        console.log(lupin);
-        newFlex.innerHTML =  newFlex.innerHTML + newFlexContent + lupin + pic3Tag;
-        lupin = lupin + 1;
-    }
-    
+pic1.onclick = function () {
+  alternatePic(pic1);
+};
+pic2.onclick = function () {
+  alternatePic(pic2);
 };
 
-button.onclick = function(){newFlexGo();newFlex.style.color='blue';this.style.color='red';backroundFlex.style.backgroundColor='red'};
+button.onclick = function () {
+  newFlexGo();
+  newFlex.style.color = "blue";
+  this.style.color = "red";
+ };
