bdf360a0a790527d876bd7d6f8f006b23e445512
Facundo Conde <facundo.conde92@gmail.com>
2023-07-19 11:32:17 -0300

    array experiments plus script clean

--- script/script.js
+++ script/script.js
@@ -1,12 +1,21 @@
-const pic1 = document.getElementById("pic-1");
-var foto = 2;
-var button = document.getElementById("button");
-var newFlex = document.getElementById("new-flex");
-var newFlexContent1 = "<div><h2>Photo number ";
-var nFc2 = "</h2></div>";
-var backroundFlex = document.getElementById("body");
-var sign = document.getElementById("sign");
+var domElements = [
+pic1 = document.getElementById("pic-1"),
+button = document.getElementById("button"),
+newFlex = document.getElementById("new-flex"),
+backroundFlex = document.getElementById("body"),
+sign = document.getElementById("sign"),
+tit = document.getElementById("doc-tit"),
+mod = document.getElementById("doc-mod"),
+wid = document.getElementById("win-wid"),
+loc = document.getElementById("win-loc")
+]
 
+var newFlexContent = [
+  nFc1 = "<div><h2>Photo number ",
+  nFc2 = "</h2></div>"
+]
+
+var foto = 2;
 function alternatePic(pic) {
   if (foto < 6) {
     pic.innerHTML =
@@ -21,16 +30,11 @@ function alternatePic(pic) {
 }
 
 var butoni = 0;
-
-function newFlexGo() 
-{
-  if (butoni % 2 === 0)
-  {
-    for (i=1; i <= 5 ; i++) 
-    {
+function newFlexGo(){
+  if (butoni % 2 === 0){
+    for (i=1; i <= 5 ; i++){
       button.innerHTML = "Hide photos";
-      console.log(i);
-      newFlex.innerHTML = newFlex.innerHTML + newFlexContent1 + i + nFc2 + '<div id="new-pic-' + i + '"><img class="pic" src="img/FOTO' + i + '.jpg" alt="pic1"></div>';
+      newFlex.innerHTML = newFlex.innerHTML + nFc1 + i + nFc2 + '<div id="new-pic-' + i + '"><img class="pic" src="img/FOTO' + i + '.jpg" alt="pic1"></div>';
     }
   } else 
   {
@@ -43,22 +47,14 @@ function newFlexGo()
 
 pic1.onclick = function () {
   sign.style.visibility="hidden"
-  /* sign.style.display="none"*/ 
   alternatePic(pic1);
-
 };
 
 button.onclick = function () {
   newFlexGo();
-  this.style.color = "blue";
 };
 
-var tit = document.getElementById("doc-tit");
-var mod = document.getElementById("doc-mod");
-var wid = document.getElementById("win-wid");
-var loc = document.getElementById("win-loc");
-
-tit.onclick = function(){ console.log(document.title); alert(document.title)}
+/* tit.onclick = function(){ console.log(document.title); alert(document.title)}
 mod.onclick = function(){ console.log(document.lastModified); alert(document.lastModified)}
 wid.onclick = function(){ console.log(window.screen.width); alert(window.screen.width)}
-loc.onclick = function(){ console.log(window.location); alert(window.location)}
+loc.onclick = function(){ console.log(window.location); alert(window.location)} */
